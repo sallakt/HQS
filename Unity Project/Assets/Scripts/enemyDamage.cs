9af22f61614ee25a317816d66170ef6861007833
Harrisonnguyen1210 <giahungftu2@gmail.com>
2018-02-06 23:35:14 +0200

    Fixing sprites, adding character health and dying effect

---
+++ Unity Project/Assets/Scripts/enemyDamage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyDamage : MonoBehaviour {
+    public float damage;
+    float damageRate = 0.5f;
+    public float pushBackForce;
+    float nextDamage;
+	// Use this for initialization
+	void Start () {
+        nextDamage = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && nextDamage < Time.time)
+        {
+            playerHealth thePlayerHealth = collision.gameObject.GetComponent<playerHealth>();
+            thePlayerHealth.addDamage(damage);
+            nextDamage = damageRate + Time.time;
+            pushBack(collision.transform);
+        }
+    }
+
+    //Create method to push the player back when touching the enemy
+    void pushBack(Transform pushedObject)
+    {
+        Vector2 pushDirection = new Vector2(0, (pushedObject.position.y - transform.position.y)).normalized;
+        pushDirection *= pushBackForce;
+        Rigidbody2D pushRigidBody = pushedObject.gameObject.GetComponent<Rigidbody2D>();
+        pushRigidBody.velocity = Vector2.zero;
+        pushRigidBody.AddForce(pushDirection, ForceMode2D.Impulse);
+    }
+}
---
+++ Unity Project/Assets/Scripts/playerHealth.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour {
+    public float maxHealth;
+    public float currentHealth;
+    public GameObject bloodEffect;
+    
+	// Use this for initialization
+	void Start () {
+        currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void addDamage(float damage)
+    {
+        if (damage <= 0)
+            return;
+        currentHealth -= damage;
+        if(currentHealth <=0)
+        {
+            makeDead();
+        }
+    }
+
+    public void makeDead()
+    {
+        Instantiate(bloodEffect, transform.position, transform.rotation);
+        gameObject.SetActive(false);
+    }
+}
